77a8c5659e54d913d86174a9d27a414a75e5a4e6
Ned <user@Neds-MacBook-Pro.local>
2023-08-16 13:47:04 -0400

    Initial commit.  Setting up Vue.

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          //treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes('-'),
+        },
+      },
+    }),
+    {
+      ...eslint({
+        include: 'src/**/*.+(js)',
+      }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});`
